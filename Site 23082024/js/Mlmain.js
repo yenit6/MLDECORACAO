bf1c0a239eb67e711e6671a6e92897bc910c7b10
Caio Justino de Jesus <144128768+CaioJustin@users.noreply.github.com>
2024-08-24 00:09:16 -0300

    Add files via upload
    atualização de 23/028/2024 pagina principal feita

---
+++ Site 23082024/js/Mlmain.js
@@ -0,0 +1,11 @@
+const simboloMenu = document.querySelector('#simbolo_menu');
+const fecharMenu = document.querySelector('#close_menu');
+const menuHeader = document.querySelector('#menu_header');
+
+simboloMenu.addEventListener('click', () => {
+    menuHeader.classList.remove('hidden');
+});
+
+fecharMenu.addEventListener('click', () => {
+    menuHeader.classList.add('hidden');
+});
