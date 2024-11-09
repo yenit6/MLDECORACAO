42684efb6ce7f726f212dc07bf5f0bfca7a7e8f0
Caio Justino de Jesus <144128768+CaioJustin@users.noreply.github.com>
2024-11-09 19:16:20 -0300

    Update Mlmain.js

--- MLDECORACAO-main/MLDECORACAO-main/produtos/Site 23082024/js/Mlmain.js
+++ MLDECORACAO-main/MLDECORACAO-main/produtos/Site 23082024/js/Mlmain.js
@@ -9,3 +9,26 @@ simboloMenu.addEventListener('click', () => {
 fecharMenu.addEventListener('click', () => {
     menuHeader.classList.add('hidden');
 });
+
+
+//Like e deslike 
+
+let like = document.getElementById('like');
+let deslike = document.getElementById('deslike');
+
+
+
+like.addEventListener('click',()=>{
+
+        like.src ='file/gostarblakc.png'
+        deslike.src='file/nao-gosto.png'
+        
+})
+
+
+deslike.addEventListener('click',()=>{
+
+    deslike.src ='file/nao-gosto(1).png'
+    like.src='file/gostar.png'
+    
+})
