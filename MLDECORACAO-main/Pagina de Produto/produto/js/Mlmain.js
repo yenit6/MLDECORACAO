e512ecb13c1b0db3cc896f463947a8524650eb66
Caio justino de jesus Santos <144128768+CaioJustin@users.noreply.github.com>
2024-11-18 18:52:24 -0300

    Add files via upload

---
+++ MLDECORACAO-main/Pagina de Produto/produto/js/Mlmain.js
@@ -0,0 +1,34 @@
+const simboloMenu = document.querySelector('#simbolo_menu');
+const fecharMenu = document.querySelector('#close_menu');
+const menuHeader = document.querySelector('#menu_header');
+
+simboloMenu.addEventListener('click', () => {
+    menuHeader.classList.remove('hidden');
+});
+
+fecharMenu.addEventListener('click', () => {
+    menuHeader.classList.add('hidden');
+});
+
+
+//Like e deslike 
+
+let like = document.getElementById('like');
+let deslike = document.getElementById('deslike');
+
+
+
+like.addEventListener('click',()=>{
+
+        like.src ='file/gostarblakc.png'
+        deslike.src='file/nao-gosto.png'
+        
+})
+
+
+deslike.addEventListener('click',()=>{
+
+    deslike.src ='file/nao-gosto(1).png'
+    like.src='file/gostar.png'
+    
+})
